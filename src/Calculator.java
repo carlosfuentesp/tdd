eb8cac3a093a9b3ee1ef67f04696e7312299c74d
Carlos Fuentes <cfuentes@thoughtworks.com>
2020-07-28 18:53:26 -0500

    Refactor add method using lambda expressions

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,15 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculator {
     public int Add(String numbers) {
-        int sum = 0;
-        if( !numbers.isEmpty() ) {
-            // String [] numbersSplit = numbers.split("//(.*?)\n");
-            // TODO: make this work
-            for (String splitNumber : numbers.split("[\\n,]+"))
-                sum = sum + Integer.parseInt(splitNumber);
-            return sum;
-        }
-
-        return 0;
-
+        List<String> numberList = Arrays.asList(numbers.split("[\\n,]+"));
+        return numberList.get(0).equals("") ? 0 : new ArrayList<>(numberList).stream().mapToInt(Integer::parseInt).sum();
     }
 }
