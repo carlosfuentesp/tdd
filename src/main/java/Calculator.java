1c5f50e87d0b406be694b89ea39237e0f902a0d9
Mavelin Ati <mavelin.ati@thoughtworks.com>
2020-08-28 18:12:22 -0500

    Add MVN project

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class Calculator {
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Arrays;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
